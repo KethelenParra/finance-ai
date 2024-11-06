1f137bc29914e060c360938d7f6ef2b018961fdf
Kethelen Parra <kethelenvictoria2016@gmail.com>
2024-11-05 22:14:00 -0300

    fix: add login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "./_components/ui/button";
-
 const Home = () => {
-  return
-  <Button>Hello world</Button>;
-
+  return (
+    <div className="flex w-screen items-center justify-center">
+      <h1 className="text-red-500">Hello world</h1>
+    </div>
+  );
 };
 
 export default Home;
